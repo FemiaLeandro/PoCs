64b93db7e0273e820318e0212cab601680fa9168
Leandro Femia <leandro.femia@endava.com>
2022-01-04 09:57:07 -0300

    Added NuGet PoCs

---
+++ net6-nugetconsumer-poc/Net6.NugetConsumer.PoC.Main/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Net6.NuGet.PoC.Services;
+using Net6.NuGet.PoC.Services.Interfaces;
+using System.Text;
+
+const string helloWorld = "Hello, World!";
+
+Console.WriteLine(helloWorld);
+
+ITestService testService = new TestServiceImplementation();
+
+Console.WriteLine(testService.Base64Encode(Encoding.UTF8.GetBytes(helloWorld)));
+
