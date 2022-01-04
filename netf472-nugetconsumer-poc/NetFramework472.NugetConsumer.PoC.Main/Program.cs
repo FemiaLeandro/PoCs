f84bf51b85634544f6b2464493646ea58ea065ab
Leandro Femia <leandro.femia@endava.com>
2022-01-04 11:19:02 -0300

    Add example of .NET Standard 2.0 NuGet and .NET Framework 4.7.2 consumer

---
+++ netf472-nugetconsumer-poc/NetFramework472.NugetConsumer.PoC.Main/Program.cs
@@ -0,0 +1,16 @@
+﻿using NetStandard2.NuGet.PoC;
+using System;
+
+namespace NetFramework472.NugetConsumer.PoC.Main
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var testClass1 = new TestClass1();
+            Console.WriteLine($"Is it a NuGet? {testClass1.IsNuGet}");
+        }
+    }
+}
